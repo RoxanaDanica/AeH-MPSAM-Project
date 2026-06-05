c2a4da9ba8c004e5b189272bbf4fe5684868a3ea
Roxana Danica <danicaroxana92@gmail.com>
2026-06-05 14:23:45 +0300

    Debug Azure deployment

--- public/index.php
+++ public/index.php
@@ -1,16 +1,3 @@
 <?php
-require_once __DIR__ . '/../app/config.php';
-
-if (!isLoggedIn()) {
-    redirect(url('login.php'));
-}
-
-// Redirect către dashboard-ul corespunzător rolului
-$user = currentUser();
-if ($user['rol'] === 'medic') {
-    redirect(url('dashboard_medic.php'));
-} elseif ($user['rol'] === 'pacient') {
-    redirect(url('dashboard_pacient.php'));
-} else {
-    redirect(url('login.php'));
-}
+    echo "Public / INDEX.PHP";
+exit;
