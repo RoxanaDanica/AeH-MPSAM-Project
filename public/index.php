15eb549650ac550a75d9a9600c6e55d4d05313e4
Roxana Danica <danicaroxana92@gmail.com>
2026-06-05 14:30:52 +0300

    debug app

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Public / INDEX.PHP";
-exit;
+
+echo "<h1>INDEX.php</h1>";
+
+require_once __DIR__ . '/../app/config.php';
+
+echo "<h1>CONFIG</h1>";
+
+if (function_exists('isLoggedIn')) {
+    echo "<h1>isLoggedIn</h1>";
+} else {
+    echo "<h1>isLoggedIn</h1>";
+}
