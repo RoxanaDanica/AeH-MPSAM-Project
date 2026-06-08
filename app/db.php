b96525be7904cab1af17303e52df04327a7167cb
Roxana Danica <danicaroxana92@gmail.com>
2026-06-08 23:51:09 +0300

    .

--- app/db.php
+++ app/db.php
@@ -23,8 +23,7 @@ function db() {
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_TIMEOUT => 5
+                PDO::ATTR_EMULATE_PREPARES => false
             ]
         );
 
