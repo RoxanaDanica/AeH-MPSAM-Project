791cbfe1fa8d8a9e64c7af1e4327a0435b501b8f
Roxana Danica <danicaroxana92@gmail.com>
2026-06-18 13:28:14 +0300

    ....

--- public/profil.php
+++ public/profil.php
@@ -1,6 +1,5 @@
 <?php
-require_once dirname(__DIR__) . '/app/config.php';
-require_once dirname( __DIR__ ) . '/app/guards.php';
+require_once __DIR__ . '/../app/bootstrap.php';
 requireLogin();
 
 $user = currentUser();
